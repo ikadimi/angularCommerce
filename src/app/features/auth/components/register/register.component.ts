42a071533507cad340cf41dfcac0b68dedcd9376
ikadimi <ilkadimi@outlook.com>
2024-09-26 15:49:16 +0200

    adding redirectio to login after successful registration

--- src/app/features/auth/components/register/register.component.ts
+++ src/app/features/auth/components/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AuthModule } from '../../shared/auth.module';
 import { AuthService } from '../../services/auth.service';
 import { RegisterationForm } from '../models/auth.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -14,13 +15,14 @@ export class RegisterComponent {
   userRegistration: RegisterationForm = { username: '', email: '', password: '' };
   isSubmitting = false;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   register() {
     this.isSubmitting = true;
     this.authService.register(this.userRegistration).subscribe({
       next: () => {
         this.isSubmitting = false;
+        this.router.navigate(['/login']);
       },
       error: () => {
         this.isSubmitting = false;
